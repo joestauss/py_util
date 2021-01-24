c285c3508c6c98f97645748e863c81338548f8a2
Joe Stauss <jstauss008@gmail.com>
2021-01-24 14:50:09 -0800

    Taggable Dataset upload
    * Uploaded taggable_dataset.py (includes TaggableDataset and TaggableRecord classes) and unit tests.

---
+++ ut.taggable_dataset.py
@@ -0,0 +1,68 @@
+import unittest
+import json
+from taggable_dataset import TaggableDataset, TaggableRecord
+
+class Test_TaggableClasses( unittest.TestCase):
+    def setUp( self):
+        dd_1   = {f'key {i}' : f'value {i}' for i in range(5)}
+        tags_1 = { f'tag {i}' for i in range(3)}
+        dd_2   = {f'key {i}' : f'alternate value {i}' for i in range(5)}
+        tags_2 = { f'tag {i+1}' for i in range(3)}
+
+        self.tr1     = TaggableRecord( dd=dd_1, tags=tags_1)
+        self.tr1_alt = TaggableRecord( dd=dd_1, tags=tags_2)
+        self.tr2     = TaggableRecord( dd=dd_2, tags=tags_2)
+
+        self.tds1    = TaggableDataset( data={self.tr1, self.tr2})
+        self.tds2    = TaggableDataset( data={self.tr1_alt, self.tr2})
+
+    def test_TaggableRecord_repr( self):
+        self.assertEqual( self.tr1, eval( repr( self.tr1)))
+
+    def test_TaggableRecord_eq( self):
+        self.assertEqual( self.tr1, self.tr1_alt)
+        self.assertTrue(  self.tr1 != self.tr2)
+
+    def test_TaggableRecord_json( self):
+        self.assertEqual( TaggableRecord( dd=json.loads( self.tr1.json)), self.tr1)
+
+    def test_TaggableRecord_hash( self):
+        self.assertEqual( hash( self.tr1), hash( self.tr1_alt))
+        self.assertTrue(  hash( self.tr1) != hash( self.tr2))
+
+    def test_TaggableRecord_tag( self):
+        has_tags  = TaggableRecord( tags={'tag A', 'tag B'})
+        gets_tags = TaggableRecord()
+        gets_tags.tag( 'tag A')
+        gets_tags.tag( 'tag B')
+        self.assertEqual( has_tags.tags, gets_tags.tags )
+
+    def test_TaggableDataset_eq( self):
+        self.assertEqual( self.tds1, self.tds2)
+
+    def test_TaggableDataset_repr( self):
+        self.assertEqual( self.tds1, eval( repr( self.tds1)))
+
+    def test_TaggableDataset_add( self):
+        temp_tds = TaggableDataset()
+        temp_tds.add( self.tr1)
+        temp_tds.add( self.tr2)
+        self.assertEqual(temp_tds, self.tds1)
+
+        temp_tds = TaggableDataset( data={TaggableRecord( tags={'A'})})
+        temp_tds.add( TaggableRecord( tags={'B'}))
+        self.assertEqual(TaggableDataset( data={TaggableRecord( tags={'A', 'B'})}).tags, temp_tds.tags)
+
+    def test_TaggableDataset_tag_all( self):
+        self.tds2.tag_all( 'TEST TAG')
+        for record in self.tds2.data:
+            self.assertTrue( 'TEST TAG' in record.tags)
+
+    def test_TaggableDataset_tags( self):
+        self.assertEqual(
+            TaggableDataset( data={TaggableRecord( tags={'A', 'B'})}).tags,
+            TaggableDataset( data={TaggableRecord( tags={'A'}), TaggableRecord(dd={'key':None}, tags={'B'})}).tags
+         )
+
+if __name__ == "__main__":
+    unittest.main()
